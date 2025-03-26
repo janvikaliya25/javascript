1ddb6567e09b1c8fb46a5ba589e5345cc10209b3
janvikaliya25 <janvikaliya@gmail.com>
2025-03-26 17:42:33 +0530

    add

---
+++ day 2/external.js
@@ -0,0 +1,5 @@
+console.log("<h1> Name:Janvi Kaliya</h1>");
+console.log("<h2>STD:Full-Stack Developmnet</h2>");
+
+console.log("Well-Come to Red and White Multimedia Education");
+alert("Well-Come to Red and White Multimedia Education")
---
+++ day 2/sum.js
@@ -0,0 +1,6 @@
+var a;
+prompt(a);
+var b;
+prompt(b);
+var c=parseInt(a)+parseInt(b);
+console.log(c);
