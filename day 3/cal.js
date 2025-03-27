81b1bcc0d9105b79ad97eb4b8670328c9c9ff544
janvikaliya25 <janvikaliya@gmail.com>
2025-03-27 10:37:28 +0530

    Add files via upload
    add

---
+++ day 3/cal.js
@@ -0,0 +1,16 @@
+var a=25;
+var b=10;
+var c=a+b;
+console.log("the addition of two number="+c);
+
+var d=a-b;
+console.log("the subtraction of two number="+d);
+
+var e=a*b;
+console.log("the multiplication of two number="+e);
+
+var f=a/b;
+console.log("the division of two number="+f);
+
+var g=a%b;
+console.log("the rimainder of two number="+g);
